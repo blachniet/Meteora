6962ce0bed9c6fe90729f763be31f8b8c89c3781
Brian Lachniet <blachniet@gmail.com>
2012-10-02 20:55:14 -0400

    Initial Commit

---
+++ Meteora.WPF/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: AssemblyTitle("Meteora.WPF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Meteora.WPF")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+//In order to begin building localizable applications, set 
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page, 
+    // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+    //(used if a resource is not found in the page, 
+    // app, or any theme specific resource dictionaries)
+)]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+
+[assembly: XmlnsPrefix("http://schemas.blachniet.com/", "bl")]
+
---
+++ Meteora.WPF/README.md
@@ -0,0 +1,32 @@
+﻿# Meteora
+
+Meteora is a dark, "don't-call-it-metro"-esque theme based on the 
+[ExpressionDark](http://wpf.codeplex.com/wikipage?title=WPF%20Themes) theme.
+
+To use the theme, put the following in the resource dictionary in your 
+application resources (`App.xaml`):
+
+```xml
+<ResourceDictionary Source="/Meteora.WPF;component/Meteora.xaml" />
+```
+
+Your final `App.xaml` may look something like this:
+
+```xml
+<Application x:Class="MyAwesomeApp.App"
+             xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+             xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+             xmlns:local="clr-namespace:MyAwesomeApp">
+    <Application.Resources>
+        <ResourceDictionary>
+            <ResourceDictionary.MergedDictionaries>
+                <ResourceDictionary Source="/Meteora.WPF;component/Meteora.xaml" />
+                <ResourceDictionary>
+                    <local:NinjectBootstrapper x:Key="bootstrapper" />
+                </ResourceDictionary>
+            </ResourceDictionary.MergedDictionaries>
+        </ResourceDictionary>
+    </Application.Resources>
+
+</Application>
+```
